3427ce9be753d9a9c92fc222d0025258d5993a8d
Naveen <catchspider2002@gmail.com>
2021-04-19 22:57:52 +0530

    Added vercel adapter

--- svelte.config.cjs
+++ svelte.config.cjs
@@ -1,6 +1,7 @@
 const sveltePreprocess = require('svelte-preprocess');
 // const node = require('@sveltejs/adapter-node');
-const static = require('@sveltejs/adapter-static');
+// const static = require('@sveltejs/adapter-static');
+const vercel = require('@sveltejs/adapter-vercel');
 const pkg = require('./package.json');
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -18,9 +19,10 @@ module.exports = {
 		// You can create optimized builds for different platforms by
 		// specifying a different adapter
 		// adapter: node(),
-		adapter: {
-			adapt: static
-		},
+		// adapter: {
+		// 	adapt: static
+		// },
+		adapter: vercel(),
 
 		// hydrate the <div id="svelte"> element in src/app.html
 		target: '#svelte',
