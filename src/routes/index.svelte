da79af39d660ccbfba024b04375c8e0d5fc42a21
Naveen <catchspider2002@gmail.com>
2021-04-19 23:06:51 +0530

    Created files for invalid path

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -10,12 +10,12 @@
 	<title>Home</title>
 </svelte:head>
 
-<section> 
+<section>
 	<h1>
 		<div class="welcome">
 			<picture>
-				<source srcset="svelte-welcome.webp" type="image/webp">
-				<img src="svelte-welcome.png" alt="Welcome"/>
+				<source srcset="svelte-welcome.webp" type="image/webp" />
+				<img src="svelte-welcome.png" alt="Welcome" />
 			</picture>
 		</div>
 
